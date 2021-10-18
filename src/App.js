9ae8b149d4040efa65339bcc71e0e3892ad5757d
Hieu Nguyen <ngochieu1993@gmail.com>
2021-10-18 17:09:27 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn forever!!
+        Learn forever!!
         </a>
       </header>
     </div>
