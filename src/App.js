016d5a9b0e9a3d7857bbeff6d3fc6dafb9417c66
Hieu Nguyen <ngochieu1993@gmail.com>
2021-10-18 22:13:11 +0200

    push to feature branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn forever!!
+        Learn forever with Nguyen!!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Learn forever!!/i);
+  const linkElement = screen.getByText(/Learn forever/i);
   expect(linkElement).toBeInTheDocument();
 });
 
